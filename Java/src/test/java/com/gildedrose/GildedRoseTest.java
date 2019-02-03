aa69620edec66cd800c6be1ad804eac484b2f34e
milindt <millind.takawale@gmail.com>
2019-02-03 19:32:38 +0530

    Done with first refactoring

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,24 @@
-package com.gildedrose;
-
-import static org.junit.Assert.*;
+package java.com.gildedrose;
 
 import org.junit.Test;
+
+import java.com.gildedrose.GildedRose;
+import java.com.gildedrose.Item;
+
+import static org.junit.Assert.*;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void assertForAgedBrieQualityIsIncreasedIfLessThan50() {
+        Item[] items = new Item[] { new Item( GildedRose.AGED_BRIE, 49, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals( 50, app.items[0].quality);
     }
 
+    @Test
+    public void name() {
+        assertFalse(true);
+    }
 }
